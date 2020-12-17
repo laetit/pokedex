2ccfe8a771b29a8d1f625db8786c254280c07224
Leticia Goncalves <leticia_99@outlook.com.br>
2020-12-17 14:51:03 -0300

    simplificacao de capitalize

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,20 +1,3 @@
 export const capitalize = (string) => {
-    let s = string.trim()
-    let upper = true
-    let resp = ''
-    s.split('').map((letra) => {
-        if (letra === ' ') {
-            resp = resp + ' '
-            upper = true
-            return false
-        }
-        if (upper) {
-            resp = resp + letra.toLocaleUpperCase()
-            upper = false
-        } else {
-            resp = resp + letra.toLocaleLowerCase()
-        }
-        return true
-    })
-    return resp
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
